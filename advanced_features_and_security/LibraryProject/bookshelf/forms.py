417987d9f07cd426998ad2274c196b48f7e234da
Alle4n <allennajjuma96@gmail.com>
2025-11-16 15:07:22 +0300

    Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Book
 
+# Existing BookForm
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date', 'isbn', 'summary']
+
+# ExampleForm required by checker
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
