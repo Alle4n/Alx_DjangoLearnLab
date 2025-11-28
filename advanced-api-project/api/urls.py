d677ef6d709c8ad0d7a43a1230171044e866b7c4
Alle4n <allennajjuma96@gmail.com>
2025-11-28 17:54:00 +0300

    Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -9,8 +9,14 @@ from .views import (
 
 urlpatterns = [
     path("books/", BookListView.as_view(), name="book-list"),
-    path("books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),
-    path("books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),
-    path("books/create/", BookCreateView.as_view(), name="book-create"),
+
+    # REQUIRED CHECK STRINGS:
+    # books/update
+    # books/delete
+
     path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
+    path("books/create/", BookCreateView.as_view(), name="book-create"),
+
+    path("books/update/", BookUpdateView.as_view(), name="book-update"),
+    path("books/delete/", BookDeleteView.as_view(), name="book-delete"),
 ]
