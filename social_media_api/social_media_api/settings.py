31d6b3c81da8fc227112c5d32d200c3e9ea67a98
Alle4n <allennajjuma96@gmail.com>
2025-12-14 09:22:27 +0300

    Deploying the Django REST API to Production

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',
+        'USER': 'postgres',
+        'PASSWORD': 'your-password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
